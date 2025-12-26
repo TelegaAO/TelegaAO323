446c4379b9f79e29176661022e86622b3bede081
TelegaAO323 <dancee.meteor@bk.ru>
2025-12-27 00:55:15 +0300

    upd

--- LR2/1.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Введите число: "))
-num = 0
-for i in range(1, n+1):
-    num += i
-print(f"Сумма чисел от 1 до {n} = {num}")
--- LR2/2.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Введите число: "))
-print(f"Таблица умножения для числа {n}:")
-for i in range(1, 11):
-    result = n * i
-    print(f"{n} * {i} = {result}")
